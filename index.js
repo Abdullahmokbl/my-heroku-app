3e3ff90fe85bc3d51b876bdded650fd4b1a9fc43
unknown <abdullahmokbl.com>
2021-03-10 20:21:23 +0200

    ff

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 // const bcrypt = require('bcryptjs')
 // const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express();
 
@@ -156,6 +157,15 @@ app.use('/auth',require('./middlewares/auth'))
 //     })
 //   })
 // })
+
+if(process.env.NODE_ENV === 'production'){
+  console.log('aaa')
+  app.use(express.static('my-appp/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'my-appp', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000
 
